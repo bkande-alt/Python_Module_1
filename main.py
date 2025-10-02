011914508d3060ae19117846ce1d8015130fc61f
Binta <bkande@eugeniaschool.com>
2025-10-02 13:13:28 +0000

    1er Dossier git

---
+++ main.py
@@ -0,0 +1,70 @@
+# print("Hello World")
+
+Nom = "Binta" 
+print(Nom)
+print(type(Nom))
+
+Age = 24
+print(Age)
+print(type(Age))
+
+Height = "1,63"
+print(Height)
+
+Height = 1.63
+print(type(Height))
+
+Is_student = "True"
+print(Is_student)
+
+
+# Viariable1 = (1 = a)
+# Variable2 = (2 = b)
+# print(variable1)
+
+Variable1 = a = 1
+Variable2 = b = 2
+print(Variable1)
+print(Variable2)
+print(type(Variable2))
+
+Vari = a + b 
+print(Vari)
+
+Nom = "Espace" 
+print(Nom)
+
+# a = 1
+# b = 2
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a % b)
+print(pow(a, b))
+
+First_name = "Binta"
+Last_name = "Kande"
+print(First_name)
+print(Last_name)
+
+print(type(First_name))
+print(type(Last_name))
+
+Full_name = First_name + " " + Last_name
+print(Full_name)
+
+Full_name = [First_name, Last_name]
+print(Full_name)
+print(type(Full_name))
+
+
+String = 16
+poulo = String
+print(int)
+print(type(int))
+
+x = 3.7
+y = int(x)
+print(y)
+print(type(y))
